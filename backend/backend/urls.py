eb0d5b8dec724a1664c28b98d9db921d5cbb76ba
Tanmay0212 <tanmayr.bhardwaj@gmail.com>
2025-02-16 18:00:34 -0500

    Tanmay's first commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+#Author Tanmay
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),            # Registration, login, profile
